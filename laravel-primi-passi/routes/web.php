e71eed39a75a065a937357cdf02f9ee458f9722c
MyUsername <my@email.com>
2023-02-07 15:01:28 +0100

    Create second page and add on route

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Homepage');
 });
+
+Route::get('/Secondpage', function () {
+    return view('Secondpage');
+});
