4783d25056080a50fa34da75de6462a05f763278
MyUsername <my@email.com>
2023-02-08 00:00:37 +0100

    Fix first character of url second page name

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('Homepage');
 });
 
-Route::get('/Secondpage', function () {
+Route::get('/secondpage', function () {
     return view('Secondpage');
 });
